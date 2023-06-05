d405f0484d35f063d41bcc74bdb9cb9f64236731
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2023-06-05 16:34:46 +0200

    1

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,4 +1,4 @@
-export const API_PATH = 'https://pixabay.com/api';
+export const API_PATH = 'https://pixabay.com/api/';
 export const API_KEY = '36974281-9a9267ae338de1504a0765e3e';
 
 export const DEFAULT_PIXABAY_PARAMS = {
